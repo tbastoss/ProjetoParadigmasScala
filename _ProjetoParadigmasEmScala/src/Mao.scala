e7553a09aad92e387b65315003de3e5373fe031e
Ranniery <Ranniery@RANNIERY-PC>
2017-08-30 21:55:22 -0300

    commit que o Thiago estava tentando

--- _ProjetoParadigmasEmScala/src/Mao.scala
+++ _ProjetoParadigmasEmScala/src/Mao.scala
@@ -10,8 +10,23 @@ class Mao (var cartas: Array[Carta]){
   
   def ordena (mao: Mao): Unit = {
     var maoAux = mao 
-    var i, j, min = 0
-    var x = new Carta('5', 'H', 0)
+    var min = 0
+    var x = new Carta('-', '-', 0)
     
+    for (i <- 0 to maoAux.getCartas().length){
+      min = i
+      for(j <- i to maoAux.getCartas().length){
+        if(maoAux.getCartas()(j).getPeso() < maoAux.getCartas()(min).getPeso()){
+          min = j
+        }
+      }
+      if(maoAux.getCartas()(i) != maoAux.getCartas()(min)){
+        x = maoAux.getCartas()(i)
+        maoAux.getCartas()(i) = maoAux.getCartas()(min)
+        maoAux.getCartas()(min) = x
+      }
+    }
+    maoAux
   }
 }
+  
